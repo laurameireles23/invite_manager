d2ac82060cf8e74af346d14755f5589bdf743163
Laura Meireles <laura.meireles1@gmail.com>
2025-01-25 16:25:01 -0300

    feat(schema): generating the schema file

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema[7.2].define(version: 2025_01_25_191904) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+end
