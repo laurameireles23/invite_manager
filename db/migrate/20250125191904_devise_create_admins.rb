68d5c0ec1fc08947109780e4b262ec9c18dd8cbe
Laura Meireles <laura.meireles1@gmail.com>
2025-01-25 16:23:13 -0300

    feat(admin): generating the admin with devise

---
+++ db/migrate/20250125191904_devise_create_admins.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class DeviseCreateAdmins < ActiveRecord::Migration[7.2]
+  def change
+    create_table :admins do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :password, null: false, default: ""
+      t.string :name,              null: false, default: ""
+
+      t.string :name
+
+      t.timestamps null: false
+    end
+
+    add_index :admins, :email,                unique: true
+  end
+end
