b1a146e928455ba2ecb647a4c42472754ca1c806
Laura Meireles <laura.meireles1@gmail.com>
2025-01-26 20:27:31 -0300

    feat(schema): running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_26_220019) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_231404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
